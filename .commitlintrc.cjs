8e2fec949844d283fe1d8a0e47d1c9ea5f58be02
Ayoub Adib <adbayb@gmail.com>
2023-03-16 08:22:57 +0100

    chore: initial commit

---
+++ .commitlintrc.cjs
@@ -0,0 +1,16 @@
+/**
+ * @see https://www.conventionalcommits.org/
+ */
+module.exports = {
+	extends: "@commitlint/config-conventional",
+	rules: {
+		"scope-enum": [
+			2,
+			"always",
+			[
+				"core",
+				"react",
+			],
+		],
+	},
+};
